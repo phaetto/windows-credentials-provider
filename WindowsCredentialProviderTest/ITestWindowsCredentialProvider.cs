eb720900616c6d3cfd4802b5abb61be28f92d5ac
Alexander Mantzoukas <alexander.mantzoukas@gmail.com>
2017-08-26 11:21:30 +0200

    Remove the excess interface definitions

--- WindowsCredentialProviderTest/ITestWindowsCredentialProvider.cs
+++ WindowsCredentialProviderTest/ITestWindowsCredentialProvider.cs
@@ -1,7 +1,5 @@
 ﻿namespace WindowsCredentialProviderTest
 {
-    using System;
-    using System.Runtime.CompilerServices;
     using System.Runtime.InteropServices;
     using CredentialProvider.Interop;
 
@@ -10,36 +8,5 @@
     [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
     public interface ITestWindowsCredentialProvider: ICredentialProvider
     {
-        [MethodImpl(MethodImplOptions.PreserveSig | MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
-        [return: MarshalAs(UnmanagedType.Error)]
-        new int SetUsageScenario([In] _CREDENTIAL_PROVIDER_USAGE_SCENARIO cpus, [In] uint dwFlags);
-
-        [MethodImpl(MethodImplOptions.PreserveSig | MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
-        [return: MarshalAs(UnmanagedType.Error)]
-        new int SetSerialization([In] ref _CREDENTIAL_PROVIDER_CREDENTIAL_SERIALIZATION pcpcs);
-
-        [MethodImpl(MethodImplOptions.PreserveSig | MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
-        [return: MarshalAs(UnmanagedType.Error)]
-        new int Advise([MarshalAs(UnmanagedType.Interface), In] ICredentialProviderEvents pcpe, [ComAliasName("CredentialProvider.Interop.UINT_PTR"), In] uint upAdviseContext);
-
-        [MethodImpl(MethodImplOptions.PreserveSig | MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
-        [return: MarshalAs(UnmanagedType.Error)]
-        new int UnAdvise();
-
-        [MethodImpl(MethodImplOptions.PreserveSig | MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
-        [return: MarshalAs(UnmanagedType.Error)]
-        new int GetFieldDescriptorCount(out uint pdwCount);
-
-        [MethodImpl(MethodImplOptions.PreserveSig | MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
-        [return: MarshalAs(UnmanagedType.Error)]
-        new int GetFieldDescriptorAt([In] uint dwIndex, [Out] IntPtr ppcpfd);
-
-        [MethodImpl(MethodImplOptions.PreserveSig | MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
-        [return: MarshalAs(UnmanagedType.Error)]
-        new int GetCredentialCount(out uint pdwCount, out uint pdwDefault, out int pbAutoLogonWithDefault);
-
-        [MethodImpl(MethodImplOptions.PreserveSig | MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
-        [return: MarshalAs(UnmanagedType.Error)]
-        new int GetCredentialAt([In] uint dwIndex, [MarshalAs(UnmanagedType.Interface)] out ICredentialProviderCredential ppcpc);
     }
 }
